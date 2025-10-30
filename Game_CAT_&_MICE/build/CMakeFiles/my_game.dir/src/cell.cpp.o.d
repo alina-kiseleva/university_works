abe56e84b4e7f55accd546f36d00634766c7288f
alina-kiseleva <freeudyst140818@mail.ru>
2025-10-30 14:17:22 +0300

    Spells added

--- Game_CAT_&_MICE/build/CMakeFiles/my_game.dir/src/cell.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/my_game.dir/src/cell.cpp.o: \
- /home/alinak1s/alishak1s/cpp_projects/my_game/src/cell.cpp \
- /usr/include/stdc-predef.h \
- /home/alinak1s/alishak1s/cpp_projects/my_game/src/../include/cell.hpp
